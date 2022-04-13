adcede7cf0c6680147b151a50f266403939607c1
BETHA\bruno.figueredo <bruno.figueredo@betha.com.br>
2022-04-13 20:03:54 -0300

    Setup Inicial

---
+++ src/main/java/com/senac/projetosocial/ProjetosocialApplication.java
@@ -0,0 +1,14 @@
+package com.senac.projetosocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class ProjetosocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/projetosocial/ProjetosocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.projetosocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
